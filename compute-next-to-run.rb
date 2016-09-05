4d92b346a3c7e79226063cca50e1704c4d172154
Maxim Malinovsky <malinovsky.dev@gmail.com>
2016-09-05 17:17:29 +0300

    Create compute-next-to-run.rb

---
+++ compute-next-to-run.rb
@@ -0,0 +1,11 @@
+require 'json'
+
+# pick at random for the sake of example.
+# replace this with your own logic
+next_profile = %w(first second third).sample
+
+puts "choosing #{next_profile}"
+
+File.open("solano-plan-variables.json", "w") do |f|
+  f.write(JSON.dump({"next_profile" => next_profile}))
+end

2cba36428a1338f72c13fc1e4ca9b6f149932f9b
Maxim Malinovsky <malinovsky.dev@gmail.com>
2016-09-07 16:19:24 +0300

    Update compute-next-to-run.rb

--- compute-next-to-run.rb
+++ compute-next-to-run.rb
@@ -1,8 +1,10 @@
 require 'json'
 
-profiles = %w(two four)
-profile = profiles.first
-
 File.open("solano-plan-variables.json", "w") do |f|
-    f.write(JSON.dump({"next_profile" => profile}))
+    if ENV['first_run_dynamic_profile'].nil? || ENV['first_run_dynamic_profile'].empty?
+        f.write(JSON.dump({"next_profile_two" => "two"}))
+        ENV['first_run_dynamic_profile'] = "fired"
+    else
+        f.write(JSON.dump({"next_profile_four" => "four"}))
+    end
 end

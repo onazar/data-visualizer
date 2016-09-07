953e02bd74c154be79e0e02851b16b2c9e28c282
Maxim Malinovsky <malinovsky.dev@gmail.com>
2016-09-07 13:04:20 +0300

    Update compute-next-to-run.rb

--- compute-next-to-run.rb
+++ compute-next-to-run.rb
@@ -1,5 +1,10 @@
 require 'json'
 
 File.open("solano-plan-variables.json", "w") do |f|
-  f.write(JSON.dump({"next_profile_two" => "two", "next_profile_four" => "four"}))
+  if !ENV['second_run']
+    f.write(JSON.dump({"next_profile_two" => "two"}))
+    ENV['second_run'] = true
+  else
+    f.write(JSON.dump({"next_profile_four" => "four"}))
+  end
 end

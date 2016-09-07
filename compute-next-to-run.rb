e745cd2988efd98da3b0e50e017c0efd83dcf2ac
Maxim Malinovsky <malinovsky.dev@gmail.com>
2016-09-07 17:40:36 +0300

    Update compute-next-to-run.rb

--- compute-next-to-run.rb
+++ compute-next-to-run.rb
@@ -3,5 +3,5 @@ require 'json'
 profile = %w(two, four).sample
 
 File.open("solano-plan-variables.json", "w") do |f|
-    f.write(JSON.dump({"next_profile" => profile}))
+    f.write(JSON.dump({"next_profile" => "#{profile}"}))
 end

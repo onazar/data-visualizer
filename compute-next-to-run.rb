039abe4aa1efd920c2551a64104b95646ae93102
Maxim Malinovsky <malinovsky.dev@gmail.com>
2016-09-07 19:59:08 +0300

    Update compute-next-to-run.rb

--- compute-next-to-run.rb
+++ compute-next-to-run.rb
@@ -1,6 +1,9 @@
 require 'json'
 
 File.open("solano-plan-variables.json", "w") do |f|
+    puts ENV["PROFILE"]
+    puts ENV["profile"]
+    puts ENV.inspect
     if ENV["PROFILE"] == "two"
         f.write(JSON.dump({"second_profile" => "two"}))
     end
